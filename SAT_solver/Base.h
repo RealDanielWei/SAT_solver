b1eba1ab8e30f7be0d16c2c9714012d2e50e5327
Zhangchao Wei <wei288@purdue.edu>
2019-04-14 17:59:04 -0400

    Basic depth-first search finished

--- SAT_solver/Base.h
+++ SAT_solver/Base.h
@@ -6,9 +6,76 @@
 
 using namespace std;
 
+enum Shader
+{
+	undecided,decided
+};
+
+struct clause_status {
+	bool value;
+	Shader status;
+};
+
+struct CNF_status {
+	bool value;
+	Shader status;
+};
+
 struct varset {
 	int num;
 	bool *values;
+	Shader *shaders;
+	varset(string s) {
+		this->num = s.length();
+		this->values = new bool[this->num];
+		this->shaders = new Shader[this->num];
+		for (int i = 0; i < this->num; i++) {
+			if (s[i] == '1') {
+				this->values[i] = true;
+				this->shaders[i] = decided;
+			}
+			else {
+				if (s[i]=='0') {
+					this->values[i] = false;
+					this->shaders[i] = decided;
+				}
+				else {
+					this->values[i] = false;
+					this->shaders[i] = undecided;
+					cout << "This varset is wrong!" << endl;
+				}
+			}
+		}
+	}
+	varset(int n) {
+		if (n <= 0) {
+			cout << "Length of varset must be positive!" << endl;
+		}
+		this->num = n;
+		this->values = new bool[this->num];
+		this->shaders = new Shader[this->num];
+		for (int i = 0; i < this->num; i++) {
+			this->values[i] = false;
+			this->shaders[i] = undecided;
+		}
+	}
+
+	void show() {
+		for (int i = 0; i < this->num; i++) {
+			if (this->shaders[i] == undecided) {
+				cout << '-';
+			}
+			else {
+				if (this->values[i] == true) {
+					cout << '1';
+				}
+				else {
+					cout << '0';
+				}
+			}
+		}
+		cout << endl;
+	}
 };
 
 class clause {
@@ -25,9 +92,44 @@ public:
 			temp.push_back(index);
 		}
 		this->termnum = temp.size();
-		this->values = (int*)malloc(this->termnum * sizeof(int));
+		this->values = new int[this->termnum];
 		for (int i = 0; i < this->termnum; i++) {
 			this->values[i] = temp[i];
+		}
+	}
+
+	bool int_to_boolean(int number, varset& vset) {
+		if (number > 0) {
+			return vset.values[abs(number) - 1];
+		}
+		else {
+			if (number < 0) {
+				return !vset.values[abs(number) - 1];
+			}
+			else {
+				cout << "This clause is wrong!" << endl;
+				return false;
+			}
+		}
+	}
+
+	clause_status evaluate(varset& vset) {
+		bool undecided_exist = false;
+		for (int i = 0; i < this->termnum; i++) {
+			if (vset.shaders[abs(this->values[i])-1] == decided) {
+				if (int_to_boolean(this->values[i],vset) == true) {
+					return clause_status{ true,decided };
+				}
+			}
+			else {
+				undecided_exist = true;
+			}
+		}
+		if (undecided_exist == true) {
+			return clause_status{ false,undecided };
+		}
+		else {
+			return clause_status{ false,decided };
 		}
 	}
 
@@ -70,9 +172,97 @@ public:
 		cout << "CNF Loading finished!" << endl;
 	}
 
+	CNF_status evaluate(varset& vset) {
+		bool false_exist = false;
+		for (int i = 0; i < this->nclause; i++) {
+			clause_status cl_status = this->clauses[i]->evaluate(vset);
+			if (cl_status.status == undecided) {
+				return CNF_status{ false,undecided };
+			}
+			else {
+				if (cl_status.value == false) {
+					false_exist = true;
+				}
+			}
+		}
+		if (false_exist == true) {
+			return CNF_status{ false,decided };
+		}
+		else {
+			return CNF_status{ true,decided };
+		}
+	}
+
 	void show() {
 		for (int i = 0; i < this->nclause; i++) {
 			(*this->clauses[i]).show();
 		}
+	}
+
+	bool DPLL_branch_search(varset& vset,int i,bool set,bool trace) {
+		vset.shaders[i] = decided;
+		vset.values[i] = set;
+		if (trace) {
+			cout << "switch to";
+			vset.show();
+		}
+		if (i == vset.num - 1) {
+			CNF_status cnfstatus = (this->evaluate(vset));
+			if (cnfstatus.status == undecided) {
+				cout << "Wrong evaluation!" << endl;
+			}
+			if (trace) {
+				cout << "The following node has been searched:" << endl;
+				vset.show();
+				cout << "The result is:" << cnfstatus.value << endl;
+			}
+			if (cnfstatus.value == false) {
+				vset.shaders[i] = undecided;
+			}
+			return cnfstatus.value;
+		}
+		if (vset.shaders[i + 1] == undecided) {
+			bool flag=this->DPLL_branch_search(vset, i + 1, false,trace);
+			if (flag == true) {
+				return true;
+			}
+			else {
+				flag = this->DPLL_branch_search(vset, i + 1, true,trace);
+				if (flag == true) {
+					return true;
+				}
+				else {
+					vset.shaders[i] = undecided;
+					return false;
+				}
+			}
+			
+			
+
+		}
+		else {
+			bool flag = this->DPLL_branch_search(vset, i + 1, vset.values[i + 1], trace);
+			if (flag == true) {
+				return true;
+			}
+			else {
+				vset.shaders[i] = undecided;
+				return false;
+			}
+		}
+
+	}
+
+	bool DPLL_search(varset& vset,bool trace) {
+		bool flag = this->DPLL_branch_search(vset, 0, false, trace);
+		if (flag == true) {
+			return true;
+		}
+		flag = this->DPLL_branch_search(vset, 0, true, trace);
+		if (flag == true) {
+			return true;
+		}
+		cout << "Unsatisfiable!" << endl;
+		return false;
 	}
 };
--- SAT_solver/main.cpp
+++ SAT_solver/main.cpp
@@ -2,10 +2,29 @@
 
 int main() {
 
-	//CNF mycnf("test.cnf");
-	CNF mycnf("bibd-8-70-35-4-15_glb.cnf");
+	CNF mycnf("test.cnf");
+	//CNF mycnf("bibd-8-70-35-4-15_glb.cnf");
+	//CNF mycnf("sdiv15prop.cnf");
 	//mycnf.show();
-
+	//varset vset("011");
+	varset vset(mycnf.nliteral);
+	mycnf.DPLL_search(vset,true);
+	vset.show();
+	/*
+	for (int i = 0; i < mycnf.nclause; i++) {
+		for (int j = 0; j < mycnf.clauses[i]->termnum; j++) {
+			cout << mycnf.clauses[i]->int_to_boolean(mycnf.clauses[i]->values[j],vset);
+		}
+		cout << endl;
+	}
+	*/
+	/*
+	for (int i = 0; i < mycnf.nclause; i++) {
+		cout << "clause_i="<<i<<" "<<mycnf.clauses[i]->evaluate(vset).value << " " << mycnf.clauses[i]->evaluate(vset).status << endl;
+	}
+	CNF_status cnf_status = mycnf.evaluate(vset);
+	cout << "CNF_value=" << cnf_status.value << " CNF_status=" << cnf_status.status << endl;
+	*/
 	system("pause");
 	return 0;
 }
